f75c8065a57707f7f1e726a64ce9bcacc81f13d6
Francisco de la Peña <frandelapena@gmail.com>
2024-07-12 11:13:24 +0200

    Fix order

--- exspy/misc/eels/tools.py
+++ exspy/misc/eels/tools.py
@@ -60,7 +60,7 @@ def _estimate_gain(
         variance2fit = variance
         average2fit = average
 
-    fit = np.polynomial.Polynomial.fit(average2fit, variance2fit, pol_order).convert().coef
+    fit = np.polynomial.Polynomial.fit(average2fit, variance2fit, pol_order).convert().coef[::-1]
     if weighted is True:
         import hyperspy.api as hs
 
